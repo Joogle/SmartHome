1402f871366371dfd5f82be3fb34cd6d3f84496e
Joogle <jannes.wendt@gmail.com>
2019-03-28 19:42:17 +0100

    Used example from snips devs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # snipsHelloWorld
 A simple Snips Action for testing.
+Part of the code is copied from https://github.com/MrJohnZoidberg/Snips-DatumUhrzeit for the time being.
+Planning on replacing it with my own as I understand what it's doing.
--- action-helloWorld.py
+++ action-helloWorld.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from hermes_python.hermes import Hermes
-import os
 
-MQTT_ADDR = "localhost:1883"        # Specify host and port for the MQTT broker
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
-def subscribe_zimmer_callback(hermes, intent_message):    # Defining callback functions to handle an intent that asks for the weather.
-    os.system('echo Hallo')
-    print("Parsed intent : {}".format(intent_message.intent.intent_name))
-    result_sentence = "Hallo Welt"
-    current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
+def intent_received(hermes, intent_message):
+    sentence = 'You asked for '
 
-with Hermes(MQTT_ADDR) as h: # Initialization of a connection to the MQTT broker
-    h.subscribe_intents(subscribe_zimmer_callback).start() \  # Registering callback functions to handle the searchWeatherForecast intent
-         
-#We get out of the with block, which closes and releases the connection
+    if intent_message.intent.intent_name == 'searchWeatherForecast':
+        print('searchWeatherForecast')
+        sentence += 'the weather '
+    elif intent_message.intent.intent_name == 'searchWeatherForecastTemperature':
+        print('searchWeatherForecastTemperature')
+        sentence += 'the temperature '
+    elif intent_message.intent.intent_name == 'searchWeatherForecastCondition':
+        print('searchWeatherForecastCondition')
+        sentence += 'the weather condition '
+    elif intent_message.intent.intent_name == 'searchWeatherForecastItem':
+        print('searchWeatherForecastItem')
+        sentence += 'the weather '
+    else:
+        return
+
+    forecast_country_slot = intent_message.slots.forecast_country.first()
+    forecast_locality_slot = intent_message.slots.forecast_locality.first()
+    forecast_start_datetime_slot = intent_message.slots.forecast_start_datetime
+
+    if forecast_locality_slot is not None:
+        sentence += 'in ' + forecast_locality_slot.value
+    if forecast_country_slot is not None:
+        sentence += 'in ' + forecast_country_slot.value
+    if forecast_start_datetime_slot is not None and len(forecast_start_datetime_slot) > 0:
+        sentence += ' ' + forecast_start_datetime_slot[0].raw_value
+
+    hermes.publish_end_session(intent_message.session_id, sentence)
+
+
+with Hermes(MQTT_ADDR) as h:
+h.subscribe_intents(intent_received).start()
