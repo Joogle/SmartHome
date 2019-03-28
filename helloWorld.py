c61f9586b2a05c20d5572fc35ba89d9b3ab8d17f
Joogle <jannes.wendt@gmail.com>
2019-03-28 19:19:08 +0100

    Added debugging echo

--- helloWorld.py
+++ helloWorld.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from hermes_python.hermes import Hermes
+import os
 
 MQTT_ADDR = "localhost:1883"        # Specify host and port for the MQTT broker
 
 
 def subscribe_zimmer_callback(hermes, intent_message):    # Defining callback functions to handle an intent that asks for the weather.
+    os.system('echo Hallo')
     print("Parsed intent : {}".format(intent_message.intent.intent_name))
     result_sentence = "Hallo Welt"
     current_session_id = intent_message.session_id
