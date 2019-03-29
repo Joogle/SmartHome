8faca134f07feed9b5ef9c7766c10c88811bede1
Joogle <jannes.wendt@gmail.com>
2019-03-29 18:15:24 +0100

    .

--- action-helloWorld.py
+++ action-helloWorld.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from hermes_python.hermes import Hermes
-from datetime import datetime
+import datetime
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -10,14 +10,14 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 name = "&APqvKWgN3mEoXVjPaPqOzQnya8J6G01wdYkOZM9r"
 
+
 def intent_received(hermes, intent_message):
     sentence = 'Hallo ich funktioniere endlich...'
     if intent_message.intent.intent_name == '&APqvKWgN3mEoXVjPaPqOzQnya8J6G01wdYkOZM9r:mDa':
-        today = datetime.date.today()
+        dateOfToday = datetime.date.today()
         dateOfReturn = datetime.date(2019, 6, 28)
-        timedelta = abs(today - dateOfReturn)
+        timedelta = abs(dateOfToday - dateOfReturn)
         sentence = 'Sie ist in' + str(timedelta.days) + 'wieder da.'
-
     hermes.publish_end_session(intent_message.session_id, sentence)
 
 
