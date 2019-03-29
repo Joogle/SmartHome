e46242892ab299a44b549fd867cd90227e36acc9
Joogle <jannes.wendt@gmail.com>
2019-03-29 17:34:41 +0100

    Changed date calculation

--- action-helloWorld.py
+++ action-helloWorld.py
@@ -10,31 +10,13 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 name = "&APqvKWgN3mEoXVjPaPqOzQnya8J6G01wdYkOZM9r"
 
-
-def days_between(d1, d2):
-    d1 = datetime.strptime(d1, "%Y-%m-%d")
-    d2 = datetime.strptime(d2, "%Y-%m-%d")
-    return abs((d2 - d1).days)
-
 def intent_received(hermes, intent_message):
     sentence = 'Hallo ich funktioniere endlich...'
-    if intent_message.intent.intent_name == '&APqvKWgN3mEoXVjPaPqOzQnya8J6G01wdYkOZM9r:mDa':
-        print('mDa')
-        sentence = 'Sie ist in' + days_between(datetime.date.isoformat(), "2019-06-28") + 'wieder da.'
-
-    # else:
-    #     return
-
-    # forecast_country_slot = intent_message.slots.forecast_country.first()
-    # forecast_locality_slot = intent_message.slots.forecast_locality.first()
-    # forecast_start_datetime_slot = intent_message.slots.forecast_start_datetime
-
-    # if forecast_locality_slot is not None:
-    #     sentence += 'in ' + forecast_locality_slot.value
-    # if forecast_country_slot is not None:
-    #     sentence += 'in ' + forecast_country_slot.value
-    # if forecast_start_datetime_slot is not None and len(forecast_start_datetime_slot) > 0:
-    #     sentence += ' ' + forecast_start_datetime_slot[0].raw_value
+    if intent_message.intent.intent_name == name + ':mDa':
+        today = datetime.date.now()
+        dateOfReturn = datetime.date(2019, 6, 28)
+        timedelta = abs(today - dateOfReturn)
+        sentence = 'Sie ist in' + timedelta.days + 'wieder da.'
 
     hermes.publish_end_session(intent_message.session_id, sentence)
 
