b0ec161140d482b2b3f8f6b484678889f316aac3
Joogle <jannes.wendt@gmail.com>
2019-03-30 15:29:59 +0100

    Added Light functionality

--- action-helloWorld.py
+++ action-helloWorld.py
@@ -9,17 +9,25 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 name = "&APqvKWgN3mEoXVjPaPqOzQnya8J6G01wdYkOZM9r"
+sentence = ''
 
 
 def intent_received(hermes, intent_message):
-    sentence = 'Hallo ich funktioniere endlich...'
-    if intent_message.intent.intent_name == '&APqvKWgN3mEoXVjPaPqOzQnya8J6G01wdYkOZM9r:mDa':
+    if intent_message.intent.intent_name == name + ':mDa':
         dateOfToday = datetime.date.today()
         dateOfReturn = datetime.date(2019, 6, 28)
         timedelta = abs(dateOfToday - dateOfReturn)
         sentence = 'Sie ist in ' + str(timedelta.days) + ' Tagen wieder da.'
+    if intent_message.intent.intent_name == name + ':Lichtsteuerung':
+        python_obj = json.loads(intent_message)
+        for slot in python_obj["slots"]:
+            if slot["slot_name"] == "Licht":
+                licht = slot["value"]["value"]
+            elif slot["slot_name"] == "an_aus":
+                an_aus = slot["value"]["value"]
+        sentence = 'Ok, mache' + licht + an_aus
     hermes.publish_end_session(intent_message.session_id, sentence)
 
-
-with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+if __name__ == "__main__":
+    with Hermes(MQTT_ADDR) as h:
+        h.subscribe_intents(intent_received).start()
